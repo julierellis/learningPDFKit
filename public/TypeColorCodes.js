84226b9afe2f5c6e7679c6b58d6d9a25d1b514f9
Julie R. Ellis <julie.r.ellis@gmail.com>
2024-04-11 09:00:01 -0400

    Recovering from changes made while trying to debug the failure to load pdf problem.. Maybe writing to a file doesn't make sense.

--- public/TypeColorCodes.js
+++ public/TypeColorCodes.js
@@ -1,22 +1,22 @@
 const colors = [
 	// 110007Apr2024 Modified hexCodes to match SS's images
-{Type: 1, hexCode: "#dc9438",	colorName:	"Tree Poppy"},
-{Type: 2, hexCode: "#a1ce52", 	colorName:	"Atlantis"},
-{Type: 3, hexCode: "#5a850f", 	colorName:	"Pesto"},
-{Type: 4, hexCode: "#10771a", 	colorName:	"Mountain Meadow"},
-{Type: 5, hexCode: "#1f4762", 	colorName:	"Venice Blue"},
-{Type: 6, hexCode: "#673388", 	colorName:	"Scampi"},
-{Type: 7, hexCode: "#90a5fa", 	colorName:	"Danube"},
-{Type: 8, hexCode: "#b4220a", 	colorName:	"Cinnabar"},
-{Type: 9, hexCode: "#821f1c", 	colorName:	"Tall Poppy"}
+{Type: 1, hexCode: "#dc9438",	colorName:	"Tree Poppy, sorta"},
+{Type: 2, hexCode: "#a1ce52", 	colorName:	"Atlantis, sorta"},
+{Type: 3, hexCode: "#5a850f", 	colorName:	"Pesto, sorta"},
+{Type: 4, hexCode: "#10771a", 	colorName:	"Mountain Meadow, sorta"},
+{Type: 5, hexCode: "#1f4762", 	colorName:	"Venice Blue, sorta"},
+{Type: 6, hexCode: "#673388", 	colorName:	"Scampi, sorta"},
+{Type: 7, hexCode: "#90a5fa", 	colorName:	"Danube, sorta"},
+{Type: 8, hexCode: "#b4220a", 	colorName:	"Cinnabar, sorta"},
+{Type: 9, hexCode: "#821f1c", 	colorName:	"Tall Poppy, sorta"}
 ]
 /*
 const descriptions = [
 	{Type: 1, typeName: "Strict Perfectionist",	typeMotivation:	"Doing the right/good thing"},
-	{Type: 2, typeName: "Considerate Helper",	typeMotivation:	"Tree Poppy"},
-	{Type: 3, typeName: "Competitive Achiever",	typeMotivation:	"Tree Poppy"},
-	{Type: 4, typeName: "Intense Creative",		typeMotivation:	"Tree Poppy"},
-	{Type: 5, typeName: "Quiet Specialist",		typeMotivation:	"Tree Poppy"},
+	{Type: 2, typeName: "Considerate Helper",	typeMotivation:	"Being liked & appreciated"},
+	{Type: 3, typeName: "Competitive Achiever",	typeMotivation:	"Outshining the rest"}, //lighten color or white the text
+	{Type: 4, typeName: "Intense Creative",		typeMotivation:	"Being unique & authentic"},
+	{Type: 5, typeName: "Quiet Specialist",		typeMotivation:	"Understanding"},
 	{Type: 6, typeName: "Loyal Skeptic",		typeMotivation:	"Being safe and belonging"},
 	{Type: 7, typeName: "Enthusiastic Visionary",	typeMotivation:	"Experiencing it all/avoiding pain"},
 	{Type: 8, typeName: "Active Controller",	typeMotivation:	"Being in control/being strong"},
